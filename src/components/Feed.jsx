2be011a38db11d7b701367828cd054dcc66f8659
Vaibhav Goel <vaibhav.goel_cs.aiml19@gla.ac.in>
2023-02-08 18:30:40 +0530

    created feed page

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,10 +1,36 @@
 import React, { useState, useEffect } from 'react';
-//test
+import { useParams } from 'react-router-dom';
+import { client } from '../client';
+import MasonryLayout from './MasonryLayout';
+import Spinner from './Spinner';
+import { feedQuery, searchQuery } from '../utils/data';
+
 
 const Feed = () => {
+    const [loading, setLoading] = useState(false);
+    const [pins, setPins] = useState(null);
+    const { categoryId } = useParams();
+    useEffect(() => {
+        setLoading(true);
+        if (categoryId) {
+            const query = searchQuery(categoryId);
+            client.fetch(query)
+                .then((data) => {
+                    setPins(data);
+                    setLoading(false);
+                })
+        } else {
+            client.fetch(feedQuery)
+                .then((data) => {
+                    setPins(data);
+                    setLoading(false);
+                })
+        }
+    }, [categoryId])
+    if (loading) return <Spinner message="We are adding new ideas to your feed" />
     return (
         <div>
-            Feed
+            {pins && <MasonryLayout pins={pins} />}
         </div>
     )
 }
