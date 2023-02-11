42fbce2181141c65e6ae1404d1b89e24bf9841bf
Vaibhav Goel <vaibhav.goel_cs.aiml19@gla.ac.in>
2023-02-11 21:10:53 +0530

    created Search Page

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,20 +2,12 @@ import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
 import { RiHomeFill } from 'react-icons/ri';
 import { IoIosArrowForward } from 'react-icons/io';
-
+import { categories } from '../utils/data';
 import logo from '../assets/logo.png';
 
 const isNotActiveStyle = 'flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize';
 const isActiveStyle = 'flex items-center px-5 gap-3 font-extrabold border-r-2 border-black transition-all duration-200 ease-in-out capitalize';
 
-const categories = [
-    { name: 'Animals' },
-    { name: 'Wallpapers' },
-    { name: 'Photography' },
-    { name: 'Gaming' },
-    { name: 'Coding' },
-    { name: 'Other' }
-]
 
 const Sidebar = ({ user, closeToggle }) => {
     const handleCloseSidebar = () => {
@@ -35,6 +27,7 @@ const Sidebar = ({ user, closeToggle }) => {
                     <h3 className='mt-2 px-5 text-base 2xl:text-xl'>Discover Categories</h3>
                     {categories.slice(0, categories.length - 1).map((category) => (
                         <NavLink to={`/category/${category.name}`} className={({ isActive }) => isActive ? isActiveStyle : isNotActiveStyle} onClick={handleCloseSidebar} key={category.name}>
+                            <img src={category.image} className="w-8 h-8 rounded-full shadow-sm" alt="category" />
                             {category.name}
                         </NavLink>
                     ))}
