ee88d9e60506c4d4c4414b66d2b0782cc5c3a907
Vaibhav Goel <vaibhav.goel_cs.aiml19@gla.ac.in>
2023-02-11 13:17:47 +0530

    provided save button on pin

--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -7,18 +7,18 @@ import { client } from '../client';
 import logo from '../assets/logo.png';
 import Pins from './Pins';
 import { userQuery } from '../utils/data';
+import { fetchUser } from '../utils/fetchUser';
 
 const Home = () => {
     const [toggleSidebar, setToggleSidebar] = useState(false);
     const [user, setUser] = useState(null);
-    const userInfo = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
+    const userInfo = fetchUser();
     const scrollRef = useRef(null);
 
     useEffect(() => {
         const query = userQuery(userInfo?.sub);
         client.fetch(query)
             .then((data) => {
-                console.log("data", data);
                 setUser(data[0]);
             })
     }, [])
